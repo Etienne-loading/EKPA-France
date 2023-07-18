4147ce9bf3a2b62efae6fde9352c1004f068afea
Etienne Pagenaud <e.pagenaud@gmail.com>
2023-07-18 16:48:39 +0200

    added view index of formations and some validations on Formation model

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -1,2 +1,7 @@
 class Formation < ApplicationRecord
+  validates :title, uniqueness: true, presence: true
+  validates :description, uniqueness: true, presence: true
+  validates :code_rncp, presence: true
+  validates :duree, presence: true
+  validates :periode, presence: true
 end
