5498880b589285bfc46c289dd79dfaaf1ac25d13
Etienne Pagenaud <e.pagenaud@gmail.com>
2023-08-01 12:02:15 +0200

    fix on  seed and model adjustment

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -1,4 +1,8 @@
 class Formation < ApplicationRecord
+  has_many :blocs, dependent: :destroy
+  belongs_to :user
+  belongs_to :organisme
+
   validates :title, uniqueness: true, presence: true
   validates :description, uniqueness: true, presence: true
   validates :code_rncp, presence: true
