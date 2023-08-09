0dd60f0a22885119e4f7d103c84a40cdde862309
Etienne Pagenaud <e.pagenaud@gmail.com>
2023-08-09 19:22:22 +0200

    added links to static pages and path

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :formations, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :blocs, only: [:index]
+  resources :formations
   resources :organismes, only: [:index, :new, :create]
   get '/catalogue', to: 'pages#catalogue', as: :catalogue
   get '/contact', to: 'pages#contact', as: :contact
   get '/mon_cpf', to: 'pages#cpf', as: :mon_cpf
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/ekpafrance', to: 'pages#ekpa', as: :ekpa
 end
